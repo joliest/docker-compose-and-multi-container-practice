d587ce8bb60434b9cd6602a4f4b6c732028e317a
Joliver Estampador <joliverestampador@gmail.com>
2024-10-24 07:44:03 +0800

    first docker compose file

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const redis = require('redis');
 
 const app = express();
-const client = redis.createClient();
+const client = redis.createClient({ 
+  // from docker compose
+  host: 'redis-server', // see docker-compose name
+  port: 6379
+});
 client.set('visits', 0);
 
 app.get('/', (req, res) => {
